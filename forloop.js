7b77a42f0dc0571c7fd6b101a84d73ddc002f40b
Vlada Svetlakova <vladas3@hotmail.com>
2020-02-08 15:16:16 -0700

    reverted to original loop

--- forloop.js
+++ forloop.js
@@ -1,14 +1,6 @@
 let fruit = ["apple", "pear", "kiwi", "plum"];
-let colors = ["red", "blue", "pink"];
-let sizes = ["fineboi", "hefty Chonk", "oh lawd", "goodness gracious"];
 
-for (s = sizes.length-1; s >= 0; s--) {
-    for (i = 0; i < colors.length; i++) {
-        for (p = 0; p < fruit.length; p++) {
-            let theColor = colors[i];
-            let theFruit = fruit[p];
-            let theSize = sizes[s];
-            console.log(theSize + " " + theColor + " " + theFruit);
-        }
-    }
+for (p = 0; p < fruit.length; p++) {
+    let theFruit = fruit[p];
+    console.log(theFruit);
 }
